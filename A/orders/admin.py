baca8e8c0006c8b6ef6ffd61c14ad295b823c02c
yegumi <yegumi003@gmail.com>
2025-02-25 18:51:06 +0330

    creating Order & OrderItem models

--- A/orders/admin.py
+++ A/orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInlines(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ("product", )
+    # Use raw_id_fields to show a text input for the 'product' field instead of selecting its id
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "paid", "updated")
+    list_filter = ("paid", )
+    inlines = (OrderItemInlines,)
+
--- A/orders/models.py
+++ A/orders/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from accounts.models import User
+from home.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    paid=models.BooleanField(default=False)
+    created=models.DateTimeField(auto_now_add=True)
+    updated =models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=("paid", "-updated")
+
+    def __str__(self):
+        return f"{self.user} - {self.id }"
+
+    def get_total_cost(self):
+        return f"{sum(item.get_cost() for item in self.order_item.all())}"
+
+
+
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_item")
+    product=models.ForeignKey(Product, on_delete=models.CASCADE, related_name="order")
+    quantity=models.IntegerField()
+    price=models.FloatField(default=1)
+
+    def __str__(self):
+        return f"{self.id}"
+
+    def get_cost(self):
+        return self.quantity * self.price
